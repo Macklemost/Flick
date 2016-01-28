19d81a72443ca5025ae43247a30d65a14c78173b
Macklemost <cmclemore@Cole-McLemores-MacBook-Pro.local>
2016-01-27 19:57:59 -0600

    Flick Functional
    All necessary features are implemented but optional ones are not.

--- Flick/MoviesViewController.swift
+++ Flick/MoviesViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
-    
+    let refreshControl = UIRefreshControl()
+    let request = NSURLRequest()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+       
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -34,9 +41,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegate: nil,
             delegateQueue: NSOperationQueue.mainQueue()
         )
-        
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+
         let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
+                MBProgressHUD.hideHUDForView(self.view, animated: true)
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
@@ -84,7 +95,69 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         print("row\(indexPath.row)")
         return cell
     }
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        // ... Create the NSURLRequest (myRequest) ...
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (data, response, error) in
+                
+                // ... Use the new data to update the data source ...
+                
+                // Reload the tableView now that there is new data
+                self.tableView.reloadData()
+                
+                // Tell the refreshControl to stop spinning
+                refreshControl.endRefreshing()	
+        });
+        task.resume() }
+    
+    /*func refreshControlAction() {
+    
+    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+    let request = NSURLRequest(
+        URL: url!,
+        cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+        timeoutInterval: 10)
+    
+    let session = NSURLSession(
+        configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+        delegate: nil,
+        delegateQueue: NSOperationQueue.mainQueue()
+    )
+    MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+    
+    
+    let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+        completionHandler: { (dataOrNil, response, error) in
+            MBProgressHUD.hideHUDForView(self.view, animated: true)
+            self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
+            if let data = dataOrNil {
+                if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                    data, options:[]) as? NSDictionary {
+                        print("response: \(responseDictionary)")
+                        
+                        self.movies = responseDictionary["results"] as! [NSDictionary]
+                        self.tableView.reloadData()
+                        
+                }
+            }
+    })
+    task.resume()
+    }
 
+*/
+    
 
     /*
     // MARK: - Navigation
@@ -96,4 +169,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     */
 
+
 }
+
+
--- Pods/Target Support Files/Pods-Flick/Pods-Flick-frameworks.sh
+++ Pods/Target Support Files/Pods-Flick/Pods-Flick-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Flick/AFNetworking.framework"
+  install_framework "Pods-Flick/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Flick/AFNetworking.framework"
+  install_framework "Pods-Flick/MBProgressHUD.framework"
 fi
